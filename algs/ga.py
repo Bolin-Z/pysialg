cffc5cee6c23b496fce80646042f7d270477638f
BolinZ <csbolinz@mail.scut.edu.cn>
2023-09-15 23:43:05 +0800

    ga v1

--- algs/ga.py
+++ algs/ga.py
@@ -1,22 +1,33 @@
 """ga.py Genetic Algorithm for TSP
 """
 from tools.debug import DEBUG
-from random import seed, uniform as rand
+from random import seed, shuffle,uniform as rand
 from tests.tsp import DirectedWeightedGraph
 
 class _Chromosome:
     def __init__(self) -> None:
-        self.gene:list[float|int] = []
+        self.gene:list[float|int] = None
         self.tourLength:float = 0.0
-        self.eval:float = 0.0
+        self.fitnessVal:float = 0.0
 
 class GeneticAlgorithm:
     def run(self) -> None:
         # initialization phase
+        self._initialPopulation()
+        self._evaluation()
+        self._updategBest()
         # main loop
         for _ in range(self.maxGeneration):
+            self._selection()
+            self._crossover()
+            self._mutation()
             self._evaluation()
-            newPopulation = self._selection()
+            self._updategBest()
+        
+        if DEBUG.ON:
+            print("---------- GA ----------")
+            self.printResult()
+            print("------------------------")
             
     def __init__(
             self,
@@ -36,19 +47,57 @@ class GeneticAlgorithm:
         self.g = graph
         # population size
         self.popSize = populationSize
-        self.pupulation = [_Chromosome() for _ in range(self.popSize)]
+        self.population = [_Chromosome() for _ in range(self.popSize)]
         # parameter
         self.pc = pc
         self.pm = pm
         # result
         self.gBestTour = []
-        self.gBestLength = None
+        self.gBestLength = float('inf')
+    
+    def _initialPopulation(self) -> None:
+        for chromosome in self.population:
+            chromosome.gene = [c for c in range(self.g.numOfCites)]
+            shuffle(chromosome.gene)
     
     def _evaluation(self) -> None:
+        for chromosome in self.population:
+            chromosome.tourLength = 0.0
+            for i in range(-1, self.g.numOfCites - 1, 1):
+                src = chromosome.gene[i]
+                dst = chromosome.gene[i + 1]
+                chromosome.tourLength += self.g.distance[src][dst]
+            chromosome.fitnessVal = 1 / chromosome.tourLength
+
+    def _selection(self) -> None:
+        p = 0.0
+        cp = []
+        for i in range(self.popSize):
+            pass
+
+    def rouletteWheel(self) -> int:
         pass
 
-    def _selection(self) -> list[_Chromosome]:
+    def _crossover(self) -> None:
         pass
 
-    def _crossover(self, population:list[_Chromosome]):
+    def _mutation(self) -> None:
         pass
+
+    def _updategBest(self) -> None:
+        curBest = self.population[0]
+        for i in range(self.popSize):
+            chromosome = self.population[i]
+            if chromosome.tourLength < curBest.tourLength:
+                curBest = self.population[i]
+        if curBest.tourLength < self.gBestLength:
+            self.gBestTour = [c for c in curBest.gene]
+            self.gBestLength = curBest.tourLength
+    
+    def printResult(self) -> None:
+        """print the best tour and its length
+        """
+        for i in self.gBestTour:
+            print(i + 1, end=" -> ")
+        print()
+        print(f"length: {self.gBestLength}")
--- main.py
+++ main.py
@@ -8,13 +8,17 @@ from algs.pso import ParticleSwarmOptimization
 
 if __name__ == "__main__":
     DEBUG.ON = True
-    D = 30
-    f = Ackley(
-        [-32.768 for _ in range(D)],
-        [ 32.768 for _ in range(D)]
-    )
-    r = time()
-    solver = DifferentialEvolution(f, populationSize=5*D,maxGeneration=2000,rdSeed=r)
-    solver.run()
-    solver = ParticleSwarmOptimization(f, populationSize=5*D,maxGeneration=2000, rdSeed=r)
-    solver.run()
+    # D = 30
+    # f = Ackley(
+    #     [-32.768 for _ in range(D)],
+    #     [ 32.768 for _ in range(D)]
+    # )
+    # r = time()
+    # solver = DifferentialEvolution(f, populationSize=5*D,maxGeneration=2000,rdSeed=r)
+    # solver.run()
+    # solver = ParticleSwarmOptimization(f, populationSize=5*D,maxGeneration=2000, rdSeed=r)
+    # solver.run()
+    a = [i for i in range(5)]
+    print(a)
+    for i in range(-1, len(a) - 1, 1):
+        print(f"{a[i]} -> {a[i + 1]}")
