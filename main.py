3fd7318a330f31ae9b89b8d7798cef49d06ba90e
Bolin-Z <csbolinz@mail.scut.edu.cn>
2023-09-16 20:25:59 +0800

    as

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from tools.debug import DEBUG
 from tests.tsp import tsp, DirectedWeightedGraph
 from algs.acs import AntColonySystem
 from algs.ga import GeneticAlgorithm
-from time import time
 
 if __name__ == "__main__":
     DEBUG.ON = True
-    graph = DirectedWeightedGraph(
+    graph1 = DirectedWeightedGraph(
         tsp[1], points2matrix=True
     )
-    rd = time()
-    solver = AntColonySystem(graph, rdSeed=rd)
-    solver.run()
-    solver = GeneticAlgorithm(graph, rdSeed=rd)
+    graph2 = DirectedWeightedGraph(tsp[2])
+
+    solver = AntColonySystem(graph2)
+    solver.run()    
+    solver = GeneticAlgorithm(graph2)
     solver.run()
